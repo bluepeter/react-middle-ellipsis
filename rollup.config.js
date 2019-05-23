f28fe8868323f3c7daa9c5240d8dea707a5ae7b7
Peter Bray <petertheyeti@gmail.com>
2019-05-23 13:06:24 -0700

    WIP move from Rollup to Webpack.

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import resolve from "rollup-plugin-node-resolve";
-import url from "rollup-plugin-url";
 
 import pkg from "./package.json";
 
@@ -26,10 +25,10 @@ export default {
     postcss({
       modules: true,
     }),
-    url(),
     babel({
       exclude: "node_modules/**",
       plugins: ["external-helpers"],
+      babelrc: false,
     }),
     resolve(),
     commonjs(),
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React, { useCallback } from "react";
+import React, { useCallback } from 'react';
 
 const Component = props => {
   const measuredParent = useCallback(node => {
     function ellipse(parentNode, childNode, txtNode) {
       const childWidth = childNode.offsetWidth,
         containerWidth = parentNode.offsetWidth;
+
       if (childWidth * 1.1 > containerWidth) {
         const str = txtNode.textContent,
           childChars = str.length,
@@ -13,14 +14,16 @@ const Component = props => {
           delEachSide = (childChars - canFit + 5) / 2,
           endLeft = Math.floor(childChars / 2 - delEachSide),
           startRight = Math.ceil(childChars / 2 + delEachSide);
+
         txtNode.textContent =
-          str.substr(0, endLeft) + "..." + str.substr(startRight);
+          str.substr(0, endLeft) + '...' + str.substr(startRight);
       }
     }
     if (node !== null) {
       const parent = node.parentNode,
-        child = parent.querySelector(".constrainedChild"),
-        txtToEllipse = parent.querySelector(".constrainedEllipse");
+        child = parent.querySelector('.constrainedChild'),
+        txtToEllipse = parent.querySelector('.constrainedEllipse');
+
       if (child !== null && txtToEllipse !== null) {
         ellipse(
           // Use the smaller width.
@@ -31,8 +34,9 @@ const Component = props => {
       }
     }
   });
+
   return (
-    <div ref={measuredParent} style={{ width: "100%" }}>
+    <div ref={measuredParent} style={{ width: '100%' }}>
       {props.children}
     </div>
   );
