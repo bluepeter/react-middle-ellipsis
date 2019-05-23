2f4fc0484a207eb98eb354d4bdd72cabe7c22bb8
Peter Bray <petertheyeti@gmail.com>
2019-05-23 13:20:03 -0700

    Move to Webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,21 @@ const config = {
       },
     ],
   },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
   resolve: {
     modules: [path.resolve("./node_modules"), path.resolve("./src")],
     extensions: [".json", ".js"],
